242ebebaa9fa5711e525f884895002c10ff35c46
Idrak Mustahsin <playbox8g@gmail.com>
2023-09-05 17:22:13 -0400

    Minor frontend fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,23 +11,53 @@ const Navbar = () => {
 
     const handleSignOut = async () => {
         let result = null,
-			error = null;
-		try {
-			result = await signOut(auth);
-		} catch (e) {
-			error = e;
-		}
+            error = null;
+        try {
+            result = await signOut(auth);
+        } catch (e) {
+            error = e;
+        }
 
         console.log({ result, error });
     }
 
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.navBrand}>
-                <img src="/logo.png" alt="Logo" className={styles.brandLogo} />
-                <span className={styles.brandName}>CeladonAI</span>
-            </div>
-            <ul className={styles.navList}>
+        <>
+            <nav className={styles.navbar}>
+                <div className={styles.navBrand}>
+                    <img src="/logo.png" alt="Logo" className={styles.brandLogo} />
+                    <span className={styles.brandName}>CeladonAI</span>
+                </div>
+                <ul className={styles.navList}>
+                    <li className={styles.navItem}>
+                        <Link href="/" className={styles.navLink}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link href="/dashboard" className={styles.navLink}>
+                            Dashboard
+                        </Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link href="/guide" className={styles.navLink}>
+                            Guide
+                        </Link>
+                    </li>
+                </ul>
+
+                <div className={styles.authButtons}>
+                    {user ?
+                        <p>Logged in as <strong onClick={handleSignOut} style={{ cursor: "pointer", textDecoration: "underline" }}>{user.email}</strong></p>
+                        :
+                        <>
+                            <Link href="/auth/login"><button className={styles.authButton}>Login</button></Link>
+                            <Link href="/auth/signup"><button className={styles.authButton}>Sign Up</button></Link>
+                        </>
+                    }
+                </div>
+            </nav>
+            <ul className={styles.navListMobile}>
                 <li className={styles.navItem}>
                     <Link href="/" className={styles.navLink}>
                         Home
@@ -44,17 +74,7 @@ const Navbar = () => {
                     </Link>
                 </li>
             </ul>
-            <div className={styles.authButtons}>
-                {user ? 
-                <p>Logged in as <strong onClick={handleSignOut} style={{cursor: "pointer", textDecoration: "underline"}}>{user.email}</strong></p> 
-                : 
-                <>
-                    <Link href="/auth/login"><button className={styles.authButton}>Login</button></Link>
-                    <Link href="/auth/signup"><button className={styles.authButton}>Sign Up</button></Link>
-                </>
-                }
-            </div>
-        </nav>
+        </>
     );
 };
 
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="rgb(83, 52, 182)" />
+        <meta
+          name="description"
+          content=""
+        />
+        <meta itemProp="name" content="CeladonAI | Personalised ChatGPT for your brand" />
+        <meta itemProp="url" content="https://www.celadonai.app/" />
+        <link rel="manifest" href="manifest.json" />
+        
         <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
 
         <script
